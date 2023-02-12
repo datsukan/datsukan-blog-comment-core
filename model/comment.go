511da65e127f68375de1e925ebbdc41ea4b35d8e
datsukan <s.datsukan@gmail.com>
2023-02-12 19:17:57 +0900

    CreatedAtをRange Keyに変更

--- model/comment.go
+++ model/comment.go
@@ -3,9 +3,9 @@ package model
 // Comment は、コメントを保持するテーブルの構造体。
 type Comment struct {
 	ID        string `dynamo:"ID,hash"`
+	CreatedAt string `dynamo:"CreatedAt,range"`
 	ArticleID string `dynamo:"ArticleID"`
 	ParentID  string `dynamo:"ParentID"`
 	UserName  string `dynamo:"UserName"`
 	Content   string `dynamo:"Content"`
-	CreatedAt string `dynamo:"CreatedAt"`
 }
