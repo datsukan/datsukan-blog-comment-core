2f73608f2bdd6d3d96da7071318aa6abd5da85cc
datsukan <s.datsukan@gmail.com>
2023-02-12 20:58:51 +0900

    hashキーを変更

--- model/comment.go
+++ model/comment.go
@@ -2,10 +2,10 @@ package model
 
 // Comment は、コメントを保持するテーブルの構造体。
 type Comment struct {
-	ID        string `dynamo:"ID,hash"`
-	CreatedAt string `dynamo:"CreatedAt,range"`
-	ArticleID string `dynamo:"ArticleID"`
+	ID        string `dynamo:"ID"`
+	ArticleID string `dynamo:"ArticleID,hash"`
 	ParentID  string `dynamo:"ParentID"`
 	UserName  string `dynamo:"UserName"`
 	Content   string `dynamo:"Content"`
+	CreatedAt string `dynamo:"CreatedAt,range"`
 }
