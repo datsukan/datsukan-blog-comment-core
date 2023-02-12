24506d1f67ec893939d36c73e02547da506857a3
datsukan <s.datsukan@gmail.com>
2023-02-12 21:22:10 +0900

    不要なrepoifを削除

--- repoif/comment.go
+++ repoif/comment.go
@@ -5,7 +5,5 @@ import "github.com/datsukan/datsukan-blog-comment-core/model"
 // BlogGoodRepository は、BlogGood テーブルを操作するための Repository インターフェイス。
 type CommentRepository interface {
 	ReadByArticleID(articleID string) ([]*model.Comment, error)
-	ReadByParentID(parentID string) ([]*model.Comment, error)
 	Create(articleID string, parentID string, name string, content string) (*model.Comment, error)
-	Delete(id string) error
 }
